1fe08c2323ac11f68b9ab2564873df6ba7a029cb
choheejin <hjcho9510@gmail.com>
2022-05-10 13:07:17 +0900

    Add: Bottom Navigation Bar

---
+++ app/src/main/java/com/example/myloginapp/HomeActivity.java
@@ -0,0 +1,77 @@
+package com.example.myloginapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.widget.LinearLayout;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class HomeActivity extends AppCompatActivity {
+    final String TAG = this.getClass().getSimpleName();
+
+    LinearLayout home_ly;
+    BottomNavigationView bottomNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        Log.d(TAG, "한눈의 갤러리에 입장하셨습니다.");
+
+        init();
+        SettingListener();
+
+        bottomNavigationView.setSelectedItemId(R.id.home);
+    }
+
+    private void init(){
+        home_ly = findViewById(R.id.home_ly);
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+    }
+
+    private void SettingListener(){
+        bottomNavigationView.setOnNavigationItemSelectedListener(new TabSelectedListener());
+    }
+
+    class TabSelectedListener implements BottomNavigationView.OnNavigationItemSelectedListener{
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            switch (menuItem.getItemId()) {
+                case R.id.tab_home: {
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.home_ly, new HomeFragment())
+                            .commit();
+                    return true;
+                }
+                case R.id.tab_map: {
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.home_ly, new MapFragment())
+                            .commit();
+                    return true;
+                }
+                case R.id.tab_heart: {
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.home_ly, new HeartFragment())
+                            .commit();
+                    return true;
+                }
+                case R.id.tab_user: {
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.home_ly, new UserFragment())
+                            .commit();
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+
+}
+
