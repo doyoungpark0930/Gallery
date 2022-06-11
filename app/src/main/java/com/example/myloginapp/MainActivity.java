66ae3ec2887d5231f57505e5ac3e7b576e55da61
OhJiwoo <74577768+Ohjiwoo-lab@users.noreply.github.com>
2022-06-11 13:37:40 +0900

    firebase를 활용한 푸시 알림 구현

--- app/src/main/java/com/example/myloginapp/MainActivity.java
+++ app/src/main/java/com/example/myloginapp/MainActivity.java
@@ -3,26 +3,16 @@ package com.example.myloginapp;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
 
-import android.app.Notification;
-import android.app.NotificationChannel;
 import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 
-import android.os.Build;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 import com.example.myloginapp.Home.HomeActivity;
-import com.google.firebase.iid.FirebaseInstanceId;
-import com.google.firebase.messaging.FirebaseMessaging;
 
 public class MainActivity extends AppCompatActivity {
-    NotificationCompat.Builder builder;
-    NotificationManager manager;
 
     private static String CHANNEL_ID = "channel1";
     private static String CHANNEL_NAME = "Channel1";
@@ -47,73 +37,14 @@ public class MainActivity extends AppCompatActivity {
         getStarted.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                builder = null;
-//
-//                Bitmap mLargeIconForNoti = BitmapFactory.decodeResource(getResources(), R.drawable.gallery);
-//
-//                PendingIntent mPendingIntent = PendingIntent.getActivity(MainActivity.this, 0,
-//                        new Intent(getApplicationContext(), MainActivity.class), PendingIntent.FLAG_UPDATE_CURRENT);
-//
-//                builder = new NotificationCompat.Builder(MainActivity.this, "default")
-//                                .setSmallIcon(R.drawable.popup_img)
-//                                .setContentTitle("알림 제목")
-//                                .setContentText("알림 내용!!")
-//                                .setDefaults(Notification.DEFAULT_VIBRATE)
-//                                .setLargeIcon(mLargeIconForNoti)
-//                                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-//                                .setDefaults(Notification.DEFAULT_VIBRATE)
-//                                .setAutoCancel(true)
-//                                .setContentIntent(mPendingIntent);
-//
-//                manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-//                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-//                    manager.createNotificationChannel(new NotificationChannel("default", "기본 채널", NotificationManager.IMPORTANCE_DEFAULT));
-//                }
-//                manager.notify(1, builder.build());
-
                 navigateToLoginActivity(); //getStarted버튼 클릭 시 LoginActivity로 이동.
                 //AndroidManifest에 <activity android:name=".LoginActivity"></activity> 추가해줘야함
                 //나머지 다른 activity도 추가할 때 androidManifest에 추가하기, 안하면 클릭시 앱 종료됨
             }
         });
-        
-        // 추가된 라인
-//        FirebaseMessaging.getInstance().subscribeToTopic("news");
-        FirebaseInstanceId.getInstance().getToken();
-    }
-
-    void Notification(){
-        NotificationManager manager;
-        NotificationCompat.Builder builder;
-        builder = null;
-
-        Bitmap mLargeIconForNoti = BitmapFactory.decodeResource(getResources(), R.drawable.gallery);
-
-        PendingIntent mPendingIntent = PendingIntent.getActivity(MainActivity.this, 0,
-                new Intent(getApplicationContext(), MainActivity.class), PendingIntent.FLAG_UPDATE_CURRENT);
-
-        builder = new NotificationCompat.Builder(MainActivity.this, "default")
-                .setSmallIcon(R.drawable.popup_img)
-                .setContentTitle("알림 제목")
-                .setContentText("알림 내용!!")
-                .setDefaults(Notification.DEFAULT_VIBRATE)
-                .setLargeIcon(mLargeIconForNoti)
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-                .setDefaults(Notification.DEFAULT_VIBRATE)
-                .setAutoCancel(true)
-                .setContentIntent(mPendingIntent);
-
-        manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            manager.createNotificationChannel(new NotificationChannel("default", "기본 채널", NotificationManager.IMPORTANCE_DEFAULT));
-        }
-        manager.notify(1, builder.build());
-
-        navigateToLoginActivity(); //getStarted버튼 클릭 시 LoginActivity로 이동.
-        //AndroidManifest에 <activity android:name=".LoginActivity"></activity> 추가해줘야함
-        //나머지 다른 activity도 추가할 때 androidManifest에 추가하기, 안하면 클릭시 앱 종료됨
 
     }
+
     void navigateToLoginActivity(){ //LoginActivity로 가는 함수
         finish();
         Intent intent = new Intent(MainActivity.this, HomeActivity.class);
